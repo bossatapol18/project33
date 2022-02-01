0ccb13066244cd40aa1514d4c76d1eec8c4d55a1
plangton1 <plangtonjanratsamee@gmail.com>
2022-02-01 16:05:28 +0700

    333
    3333

--- standard_head/index.php
+++ standard_head/index.php
@@ -37,9 +37,9 @@
                 include('../logout/index.php');
                 }
                 ?>
-                                        </div>
+                                       
     <?php include('./include/footer.php'); ?>
-
+ </div>
     <div id="preloader"></div>
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
     <?php include('./include/script.php'); ?>
